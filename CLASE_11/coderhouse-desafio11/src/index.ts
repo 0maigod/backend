436a46fe45423c112cd21a1d82bc52a4bb620e2f
0maigod <easterisco@gmail.com>
2021-02-13 14:07:53 -0300

    Desafio con EJS, y cambio menor en desafio 10

--- CLASE_11/coderhouse-desafio11/src/index.ts
+++ CLASE_11/coderhouse-desafio11/src/index.ts
@@ -3,30 +3,20 @@ import path from 'path'
 import { Producto } from './producto'
 import {CommonRoutesConfig} from './rutas/common.route.config'
 import {UsersRoutes} from './rutas/users.route.config'
-import handlebars from 'express-handlebars'
+
 
 const routes: Array<CommonRoutesConfig> = []
 const app = express()
 
+app.set('views', path.join(__dirname, '..', 'views'))
+app.set('view engine', 'ejs')
 
 let productos: Producto [] = []
 routes.push(new UsersRoutes(app, productos))
 
 app.use(express.static('public'))
 
-app.engine(
-    "hbs", 
-    handlebars({
-        extname: ".hbs",
-        defaultLayout: "visualizar.hbs", 
-        layoutsDir: path.join(__dirname,  '..', 'views', 'layouts'),
-        partialsDir: path.join(__dirname, '..', 'views', 'partials')
-    })
-)
 
-app.set('views', path.join(__dirname, '..', 'views'))
-app.set('view engine', 'hbs')
-
-app.listen(9090, () => {
+app.listen(8080, () => {
     console.log('escuchando en 8080')
 }).on('error', console.log);
