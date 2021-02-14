e47b66d07efac035429f9cdd94ce03e3344fad20
0maigod <easterisco@gmail.com>
2021-02-14 20:34:36 -0300

    Desafio Pug terminado

---
+++ CLASE_11/coderhouse-desafio11-pug/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import path from 'path'
+import { Producto } from './producto'
+import {CommonRoutesConfig} from './rutas/common.route.config'
+import {UsersRoutes} from './rutas/users.route.config'
+
+
+const routes: Array<CommonRoutesConfig> = []
+const app = express()
+
+app.set('views', path.join(__dirname, '..', 'views'))
+app.set('view engine', 'pug')
+
+let productos: Producto [] = []
+routes.push(new UsersRoutes(app, productos))
+
+app.use(express.static('public'))
+
+
+app.listen(8080, () => {
+    console.log('escuchando en 8080')
+}).on('error', console.log);
